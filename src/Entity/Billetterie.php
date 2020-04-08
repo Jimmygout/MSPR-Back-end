f33648f679689a05c4e54f6475bb9ef93e8a2cad
jimmy gout <jimmy.gout@hotmail.com>
2020-04-08 12:05:25 +0200

    ajout gestion image billeterie avec Vich

--- src/Entity/Billetterie.php
+++ src/Entity/Billetterie.php
@@ -2,10 +2,13 @@
 
 namespace App\Entity;
 
+use Symfony\Component\HttpFoundation\File\File;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BilletterieRepository")
+ * @Vich\Uploadable()
  */
 class Billetterie
 {
@@ -33,8 +36,39 @@ class Billetterie
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @var \DateTime
      */
     private $image;
+
+
+     /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
 
     public function getId(): ?int
     {
@@ -89,6 +123,18 @@ class Billetterie
     public function setImage(string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
